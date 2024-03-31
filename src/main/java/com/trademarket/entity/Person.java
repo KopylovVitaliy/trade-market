86fbdafe290a805af4a1bfafdf20f4e01dbc2371
vitaliy <vita-kopy@yandex.ru>
2024-03-31 12:38:00 +0300

    промежуточный

--- src/main/java/com/trademarket/entity/Person.java
+++ src/main/java/com/trademarket/entity/Person.java
@@ -1,8 +1,12 @@
 package com.trademarket.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.trademarket.model.constants.Role;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.awt.*;
+import java.util.List;
+import java.util.UUID;
 
 @Entity
 @AllArgsConstructor
@@ -12,14 +16,26 @@ import lombok.*;
 @Builder
 public class Person {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private UUID id;
     @Column(unique = true)
     private String email;
-    private String name;
-    private String surName;
-    @JsonIgnore
+    private String firstName;
+    private String secondName;
     private String password;
+    private String phone;
 
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "image_id")
+    private Image image;
+
+    @Column(length = 5, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Advertisement> advertisements;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    private List<Comment> comments;
 
 }
